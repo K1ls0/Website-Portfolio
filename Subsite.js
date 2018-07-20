1d767239598ecc92d1d824f1579a6ea26bcea841
Kilso4dev <asderuiner@gmail.com>
2018-07-21 01:55:25 +0200

    made Subsites working (subsistencial)

--- Subsite.js
+++ Subsite.js
@@ -1,12 +1,29 @@
+var subsiteContent = "";
+
 function loadSubsite() {
   let activeSubSite = $("#content").attr("data-activesubsite");
+  readFile("subsites/home.shtml");
+  console.log(subsiteContent);
+  $("#content").html(subsiteContent);
+}
 
-  let subsiteText = readFiles("subsites/home.shtml");
-  $("#content").html(subsiteText);
+function handleSubSites() {
+
 }
 
 //let map = {["home", "subsites/home.shtml"]};
 //TODO
-function readFiles(filePath) {
-
+function readFile(filePath) {
+  request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == XMLHttpRequest.DONE) {
+      if (request.status == 200) {
+        subsiteContent = request.response;
+      } else {
+        subsiteContent = "<h1>Error " + request.status + "; Couldn't find Subsite.</h1>";
+      }
+    }
+  };
+  request.open("GET", filePath, true);
+  request.send();
 }
